1461ba2a1e7a061f30b988af45921fbd585866aa
Rubal-code <rubal3232@gmail.com>
2025-12-31 21:41:30 +0530

    crop prediction

---
+++ crop prediction/templates/app.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np # type: ignore
+from flask import Flask,request,jsonify,render_template # type: ignore
+
+flask_app=Flask(__name__)
+model=pickle.load(open("model.pkl",'rb'))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict",methods=["POST"])
+def predict():
+    float_feature=[float(x) for x in request.form.values()]
+    features=[np.array(float_feature)]
+    prediction=model.predict(features)
+    return render_template("index.html",prediction_text="The Predicted crop is {}".format(prediction))
+
+if __name__=="__main__":
+    flask_app.run(debug=True)
